d876b1aaf1bbb200071ea77edb1597b655a58e91
k-haechan <gibbm1127@naver.com>
2025-06-16 18:30:27 +0900

    good

---
+++ src/main/java/com/mysite/templateproject/TemplateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.templateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/templateproject/TemplateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.templateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
